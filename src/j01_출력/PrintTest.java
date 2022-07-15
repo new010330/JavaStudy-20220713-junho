341963acbb767f71b8d61c69d9fc0d7b64377458
new010330 <rlarudtjs74a@naver.com>
2022-07-15 11:06:06 +0900

    챕터1

---
+++ src/j01_출력/PrintTest.java
@@ -0,0 +1,11 @@
+package j01_출력;
+
+public class PrintTest {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Java!");
+		System.out.println("고준호");
+
+	}
+
+}
---
+++ src/j01_출력/Test.java
@@ -0,0 +1,17 @@
+package j01_출력;
+
+import java.util.Scanner;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		int n = 5;
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+			System.out.print("*");
+		}
+		System.out.println();
+		}
+	}
+}
